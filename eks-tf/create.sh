614bcbd69de7f11c3743672591675fad4e8da922
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-18 08:12:58 +0530

    Kubernetes

---
+++ eks-tf/create.sh
@@ -0,0 +1,5 @@
+terraform init
+terraform apply -auto-approve
+
+aws eks update-kubeconfig --name example
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
