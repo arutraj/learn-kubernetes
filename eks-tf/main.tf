cb92e3795d9220d23e82b3f16aee2e61b10ffcad
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-21 08:51:20 +0530

    Kubernetes

--- eks-tf/main.tf
+++ eks-tf/main.tf
@@ -179,6 +179,27 @@ resource "aws_iam_role" "test_role" {
   }
 }
 
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "eks-pod-identity-example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
 resource "aws_eks_pod_identity_association" "example" {
   cluster_name    = aws_eks_cluster.example.name
   namespace       = "default"
