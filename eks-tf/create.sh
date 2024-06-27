b621ef5efa286866f30a955658102e07dc2e4500
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-27 07:45:05 +0530

    Kubernetes

--- eks-tf/create.sh
+++ eks-tf/create.sh
@@ -4,4 +4,6 @@ terraform apply -auto-approve
 aws eks update-kubeconfig --name example
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl apply -f cluster-autoscale.yaml
+kubectl create ns argocd
+kubectl apply -n argocd -f ../argo.yaml
 
